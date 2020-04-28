e8de53c271efc4c24bfa9c72839991dbcbcaf5fc
laperon <zhehllov@gmail.com>
2020-04-28 12:58:59 +0300

    bug fix

--- scripts.php
+++ scripts.php
@@ -11,12 +11,12 @@ function ot_scripts_styles() {
 	wp_enqueue_style( 'styles-css', get_template_directory_uri() . '/assets/css/styles.css', array(), '', false );
 
 	// Loads scripts
-	wp_enqueue_script('slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array( 'jquery' ), '', false);
-	wp_enqueue_script('uikit-js', get_template_directory_uri() . '/assets/js/uikit.min.js', array(), '', false);
-	wp_enqueue_script('matchheight-js', get_template_directory_uri() . '/assets/js/jquery.matchHeight-min', array( 'jquery' ), '', false);
-	wp_enqueue_script('typed-js', get_template_directory_uri() . '/assets/js/typed.min.js', array(), '', false);
-	wp_enqueue_script('countup-js', get_template_directory_uri() . '/assets/js/jquery.countup.min.js', array(), '', false);
-	wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array( 'jquery' ), '', false);
-	wp_enqueue_script('scripts-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', false);
+	wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/assets/js/slick.min.js', array( 'jquery' ), '', false );
+	wp_enqueue_script( 'uikit-js', get_template_directory_uri() . '/assets/js/uikit.min.js', array(), '', false );
+	wp_enqueue_script( 'matchheight-js', get_template_directory_uri() . '/assets/js/jquery.matchHeight-min', array( 'jquery' ), '', false );
+	wp_enqueue_script( 'typed-js', get_template_directory_uri() . '/assets/js/typed.min.js', array(), '', false );
+	wp_enqueue_script( 'countup-js', get_template_directory_uri() . '/assets/js/jquery.countup.min.js', array(), '', false );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array( 'jquery' ), '', false );
+	wp_enqueue_script( 'scripts-js', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), '', false ;
 }
 add_action( 'wp_enqueue_scripts', 'ot_scripts_styles' );
